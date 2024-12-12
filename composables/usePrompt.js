6f6f0197475f7ba8f6c66eeed6ab9ac11a8aa63b
Filip <f.rakowski@hotmail.com>
2024-12-12 17:57:51 +0100

    useprompt

--- composables/usePrompt.js
+++ composables/usePrompt.js
@@ -1,14 +1,20 @@
 import OpenAI from 'openai';
 import { useRuntimeConfig } from '#app';
 
-export const usePrompt = () => {
+export const usePrompt = async (prompt) => {
   const config = useRuntimeConfig();
-  
+
   const openai = new OpenAI({
     apiKey: config.openaiApiKey
   });
+  
+  const completion = await openai.chat.completions.create({
+  model: "gpt-4o-mini",
+  messages: [{   
+          role: "system", 
+          content: prompt
+      }]
+  });
 
-  return {
-    openai
-  };
+  return completion?.choices[0]?.message
 }; 
